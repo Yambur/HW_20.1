25417bd92dde45d6930a6bc12140037e437a9554
bargest <DIZz92@mail.ru>
2024-01-16 00:00:12 +0300

    настроил модели

--- main/models.py
+++ main/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=250, verbose_name='категория')
+    description = models.TextField(max_length=300, verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.name}{self.description}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=250, verbose_name='наименование')
+    description = models.TextField(max_length=300, verbose_name='описание')
+    image = models.ImageField(upload_to='main', **NULLABLE, verbose_name='фото')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
+    price = models.IntegerField(verbose_name='цена за штуку')
+    data_create = models.DateTimeField(**NULLABLE, verbose_name='дата создания')
+    data_last_add = models.DateTimeField(**NULLABLE, verbose_name='дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name} {self.description}{self.image}{self.category}{self.price}{self.data_create}{self.data_last_add}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
