4c263b74ffae997570393df0567beb5102ef5e7f
bargest <DIZz92@mail.ru>
2024-01-21 01:38:56 +0300

    add all +-

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
+from main.apps import MainConfig
 from main.views import index, contacts
 
+app_name = MainConfig.name
+
 urlpatterns = [
-    path('', index),
-    path('contacts/', contacts)
+    path('', index, name='index'),
+    path('contacts/', contacts, name='contacts')
 ]
