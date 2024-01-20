fabb6c2efc22898e1c02226949daf9ebee591146
bargest <DIZz92@mail.ru>
2024-01-20 23:49:32 +0300

    continue

--- main/management/commands/gor.py
+++ main/management/commands/gor.py
@@ -1,7 +1,8 @@
 from django.core.management import BaseCommand
 from django.db import connection
 
-from main.models import Category
+from django.db import reset_queries
+from main.models import Category, Product
 
 
 class Command(BaseCommand):
@@ -15,12 +16,33 @@ class Command(BaseCommand):
 
         Category.objects.all().delete()
 
-        # не могу понять, где изменять id
-        #with connection.cursor() as cursor:
-        #    cursor.execute("ALTER SEQUENCE category_id RESTART WITH 1")
+        with connection.cursor() as cursor:
+            cursor.execute("ALTER SEQUENCE main_category_id_seq RESTART WITH 1")
 
         categories_to_create = []
         for category in category_list:
             categories_to_create.append(Category(**category))
 
         Category.objects.bulk_create(categories_to_create)
+
+        products_list = [
+            {'name': 'Кабачок', 'description': 'Просто кабачок',
+             'category': categories_to_create[2], 'price': '50'},
+            {'name': 'Картошка', 'description': 'С голубыми глазками',
+             'category': categories_to_create[0], 'price': '100'},
+            {'name': 'Папая', 'description': 'Знать бы что это',
+             'category': categories_to_create[1], 'price': '300'},
+            {'name': 'Томатный сок', 'description': 'Вкусный, но не всем заходит',
+             'category': categories_to_create[3], 'price': '120'},
+        ]
+
+        Product.objects.all().delete()
+
+        with connection.cursor() as cursor:
+            cursor.execute("ALTER SEQUENCE main_product_id_seq RESTART WITH 1")
+
+        products_to_create = []
+        for product in products_list:
+            products_to_create.append(Product(**product))
+
+        Product.objects.bulk_create(products_to_create)
--- main/views.py
+++ main/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from main.models import Product
+
 
 def index(request):
+    product_list = Product.objects.all()
+    context = {
+        'objects_list': product_list
+    }
     return render(request, 'main/index.html')
