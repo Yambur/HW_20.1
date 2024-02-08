6fb3369026a11bcc1de2ef1962f1ccbbd3133e8d
bargest <DIZz92@mail.ru>
2024-02-09 02:31:45 +0300

    add all sh..t

--- main/urls.py
+++ main/urls.py
@@ -2,12 +2,15 @@ from django.urls import path
 
 from main.apps import MainConfig
 from main.views import IndexView, ContactsView, BlogListView, BlogCreateView, BlogUpdateView, BlogDetailView, \
-    BlogDeleteView, toggle_activity
+    BlogDeleteView, toggle_activity, ProductCreateView, ProductListView, ProductUpdateView
 
 app_name = MainConfig.name
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('product/', ProductListView.as_view(), name='product'),
+    path('product/create/', ProductCreateView.as_view(), name='product_create'),
+    path('<int:pk>/product/update/', ProductUpdateView.as_view(), name='product_update'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
     path('blog/', BlogListView.as_view(), name='blog'),
     path('blog/<int:pk>', BlogDetailView.as_view(), name='blog_detail'),
