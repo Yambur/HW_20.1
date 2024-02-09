afae521c9b489f339445beef231ac3ef8a01e51e
bargest <DIZz92@mail.ru>
2024-02-09 12:16:37 +0300

    add watch product

--- main/urls.py
+++ main/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 
 from main.apps import MainConfig
 from main.views import IndexView, ContactsView, BlogListView, BlogCreateView, BlogUpdateView, BlogDetailView, \
-    BlogDeleteView, toggle_activity, ProductCreateView, ProductListView, ProductUpdateView
+    BlogDeleteView, toggle_activity, ProductCreateView, ProductListView, ProductUpdateView, CategoryListView
 
 app_name = MainConfig.name
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('product/', ProductListView.as_view(), name='product'),
+    path('<int:pk>/product/', ProductListView.as_view(), name='product_list'),
+    path('category/', CategoryListView.as_view(), name='category_list'),
     path('product/create/', ProductCreateView.as_view(), name='product_create'),
     path('<int:pk>/product/update/', ProductUpdateView.as_view(), name='product_update'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
